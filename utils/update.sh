d2f4e2bb2c4a779eacc2e2069ea0ac33fd3ce938
Christoffer <netravnen@gmail.com>
2018-08-09 14:24:36 +0000

    Add utils/update-frr.sh

--- utils/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd ~/LabConf/hosts/$HOSTNAME/ && \
-git fetch origin master && \
-git pull origin master && \
-vtysh -c 'sh run' > ~/LabConf/hosts/$HOSTNAME/frr.conf && \
-git commit -a -m "Updated $HOSTNAME frr.conf" frr.conf && \
-git push origin master
