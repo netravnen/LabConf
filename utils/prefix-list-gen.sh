9f2b54a32196ae8561ed2454df99533fca0f28fe
Christoffer <netravnen@gmail.com>
2018-08-11 13:29:51 +0000

    Added new util prefix-list-gen.sh

---
+++ utils/prefix-list-gen.sh
@@ -0,0 +1,2 @@
+bgpq3 -3 -4 -A -D -l vultr-only-v4 -h rr.ntt.net -S RIPE AS-CHOOPA > ~/LabConf/prefix-list/vultr-only-v4.list
+bgpq3 -3 -6 -A -D -l vultr-only-v6 -h rr.ntt.net -S RIPE AS-CHOOPA > ~/LabConf/prefix-list/vultr-only-v6.list
