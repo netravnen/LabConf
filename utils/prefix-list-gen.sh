1da93ed32dddefee2c8a33a28746235a583aed40
Christoffer <netravnen@gmail.com>
2018-08-11 18:58:31 +0000

    Updated prefix-len-gen.sh

--- utils/prefix-list-gen.sh
+++ utils/prefix-list-gen.sh
@@ -1,2 +1,2 @@
-bgpq3 -3 -4 -A -D -l vultr-only-v4 -h rr.ntt.net -S RIPE AS-CHOOPA > ~/LabConf/prefix-list/vultr-only-v4.list
-bgpq3 -3 -6 -A -D -l vultr-only-v6 -h rr.ntt.net -S RIPE AS-CHOOPA > ~/LabConf/prefix-list/vultr-only-v6.list
+bgpq3 -3 -4 -A -D -l vultr-only-v4 -m 24 -h rr.ntt.net AS-CHOOPA > ~/LabConf/prefix-list/vultr-only-v4.list
+bgpq3 -3 -6 -A -D -l vultr-only-v6 -m 48 -h rr.ntt.net AS-CHOOPA > ~/LabConf/prefix-list/vultr-only-v6.list
