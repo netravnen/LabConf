01d689c9960333c73dc75eddebe411f154084cc7
Christoffer <netravnen@gmail.com>
2018-08-11 13:57:52 +0000

    Update update-frr.sh

--- utils/update-frr.sh
+++ utils/update-frr.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cd ~/LabConf/ && \
+cd ~/LabConf/hosts/$HOSTNAME/ && \
 git checkout master
 
 git fetch origin master && \
 git pull origin master && \
-vtysh -c 'sh run' > ./hosts/$(echo $HOSTNAME)/etc/frr/frr.conf && \
-git add ./hosts/$(echo $HOSTNAME)/etc/frr/frr.conf && \
-git commit -m "Updated $(echo $HOSTNAME) ./hosts/$(echo $HOSTNAME)/etc/frr/frr.conf" ./hosts/$(echo $HOSTNAME)/etc/frr/frr.conf && \
+vtysh -c 'sh run' > etc/frr/frr.conf && \
+git add etc/frr/frr.conf && \
+git commit -m "Updated $HOSTNAME etc/frr/frr.conf" etc/frr/frr.conf && \
 git push origin master
