a42abf20377bd3ce7157eb359af52eaaeba3364e
Christoffer <netravnen@gmail.com>
2018-08-11 13:26:01 +0000

    frr.conf: moved to absolut paths under ./hosts/$HOST/etc/frr utils/update-frr.sh also updated to reflect this new path

--- utils/update-frr.sh
+++ utils/update-frr.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-cd ~/LabConf/hosts/$HOSTNAME/ && \
-git checkout master
-git fetch origin master && \
-git pull origin master && \
-vtysh -c 'sh run' > ~/LabConf/hosts/$HOSTNAME/frr.conf && \
-git commit -a -m "Updated $HOSTNAME frr.conf" frr.conf && \
-git push origin master
+cd ~/LabConf/hosts/bart/ && git checkout master
+git fetch origin master && git pull origin master && vtysh -c 'sh run' > ~/LabConf/hosts/bart/etc/frr/frr.conf && git commit -m "Updated bart ./etc/frr/frr.conf" frr.conf && git push origin master
